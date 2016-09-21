a9636e9368f9ca6ea63b49d6eecacc42fe7bdd38
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2016-09-21 17:57:16 +0200

    Wrote description of the way forward.
    This problem, if not actually intractible, is surprisingly challenging to approach in
    Rust. I've come up with what might be a solution, but I have to actually try it tomorrow to see
    if it works out.

--- src/casts.rs
+++ src/casts.rs
@@ -3,63 +3,74 @@
 //! We're going to need to cast a bunch of u8 types as other data-types; this module is
 //! designed to limit the unsafety to the extent possible.
 
+use std::io;
+use std::io::Read;
+
 // We need a parameterizied way to actually get values out of a stream of u8s, so
 // let's set up a trait here.
-trait ValueExtractor<R: Read, T> {
+pub trait ValueExtractor<R: Read, T> {
     fn extract(&mut self) -> io::Result<T>;
 }
 
-// single implementation--TODO: replace with a macro for all appropriate types
+// Implementations of ValueExtractor.
+//
+// I'd really wanted to macroize this, but it looks like this is simply impossible.
+// Rust's macros aren't doing simple text substitution, but something a little more
+// deeply tied into the type system, and all of the ways I could think of to macroize
+// this require simple text substitution. Oh well, it's not that terrible to just
+// customize the copy/paste here.
 impl<R: Read> ValueExtractor<R, u8> for R {
     fn extract(&mut self) -> io::Result<u8> {
         let mut data_buf = [0; 1];
         try!(self.read_exact(&mut data_buf));
-        Ok(casts::as_u8(data_buf))
+        Ok(data_buf[0])
     }
 }
 
-/// How many bytes are contained in certain primitives.
-///
-/// Honestly surprised that this isn't apparently in std, but
-/// the closest thing I could find was std::mem::size_of, which
-/// isn't a macro, and so isn't appropriate for this use case.
-macro_rules! bytes_of {
-    (u8) => (1);
-    (i8) => (1);
-    (i16) => (2);
-    (i32) => (4);
-    (f32) => (4);
-    (f64) => (8);
+impl<R: Read> ValueExtractor<R, i8> for R {
+    fn extract(&mut self) -> io::Result<i8> {
+        let mut data_buf = [0; 1];
+        try!(self.read_exact(&mut data_buf));
+        Ok(data_buf[0] as i8)
+    }
 }
 
-/// Detailed transformation implementation
-///
-/// Here we just match the data type and perform the appropriate transformation
-macro_rules! xform {
-    ( $buf:ident, u8) => ($buf[0]);
-    ( $buf:ident, i8) => ($buf[0] as i8);
-    ( $buf:ident, i16) => (i16::from_be(unsafe { ::std::mem::transmute($buf) }));
-    ( $buf:ident, i32) => (i32::from_be(unsafe { ::std::mem::transmute($buf) }));
-    ( $buf:ident, f32) => (unsafe { ::std::mem::transmute($buf) });
-    ( $buf:ident, f64) => (unsafe { ::std::mem::transmute($buf) });
+impl<R: Read> ValueExtractor<R, i16> for R {
+    fn extract(&mut self) -> io::Result<i16> {
+        let mut data_buf = [0; 2];
+        try!(self.read_exact(&mut data_buf));
+        Ok(i16::from_be(unsafe { ::std::mem::transmute(data_buf) }))
+    }
 }
 
-/// Automatically implement ValueExtractor for the given type.
-///
-/// Only works for types for which we've implemented appropriate cases
-/// of `bytes_of!($x)` and `casts::as_$x()`.
-macro_rules! ve_impl {
-    ( $( $x:ty ),* ) => (
-        $(
-            impl<R: Read> ValueExtractor<R, $x> for R {
-                fn extract(&mut self) -> io::Result<$x> {
-                    let mut data_buf = [0; bytes_of!($x)];
-                    try!(self.read_exact(&mut data_buf));
-                    Ok(xform!(data_buf, $x))
-                }
-            }
-        )*
-    );
+impl<R: Read> ValueExtractor<R, u32> for R {
+    fn extract(&mut self) -> io::Result<u32> {
+        let mut data_buf = [0; 4];
+        try!(self.read_exact(&mut data_buf));
+        Ok(u32::from_be(unsafe { ::std::mem::transmute(data_buf) }))
+    }
 }
 
-ve_impl!{i8, i16, i32, f32, f64}
+impl<R: Read> ValueExtractor<R, i32> for R {
+    fn extract(&mut self) -> io::Result<i32> {
+        let mut data_buf = [0; 4];
+        try!(self.read_exact(&mut data_buf));
+        Ok(i32::from_be(unsafe { ::std::mem::transmute(data_buf) }))
+    }
+}
+
+impl<R: Read> ValueExtractor<R, f32> for R {
+    fn extract(&mut self) -> io::Result<f32> {
+        let mut data_buf = [0; 4];
+        try!(self.read_exact(&mut data_buf));
+        Ok(unsafe { ::std::mem::transmute(data_buf) })
+    }
+}
+
+impl<R: Read> ValueExtractor<R, f64> for R {
+    fn extract(&mut self) -> io::Result<f64> {
+        let mut data_buf = [0; 8];
+        try!(self.read_exact(&mut data_buf));
+        Ok(unsafe { ::std::mem::transmute(data_buf) })
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -3,43 +3,7 @@ use std::io::Read;
 use std::marker::PhantomData;
 
 mod casts;
-
-/// The type of data represented in this .idx file.
-///
-/// A given file will always contain a single data type.
-/// The type is specified in the third byte of the file:
-///
-/// ```text
-/// 0x08: unsigned byte
-/// 0x09: signed byte
-/// 0x0B: short (2 bytes)
-/// 0x0C: int (4 bytes)
-/// 0x0D: float (4 bytes)
-/// 0x0E: double (8 bytes)
-/// ```
-#[derive(Debug, PartialEq, Clone, Copy)]
-pub enum DataType {
-    U8,
-    I8,
-    I16,
-    I32,
-    F32,
-    F64,
-}
-
-impl DataType {
-    /// How many bytes does this datatype take?
-    pub fn bytes(&self) -> usize {
-        match *self {
-            DataType::U8 => 1,
-            DataType::I8 => 1,
-            DataType::I16 => 2,
-            DataType::I32 => 4,
-            DataType::F32 => 4,
-            DataType::F64 => 8,
-        }
-    }
-}
+use casts::ValueExtractor;
 
 #[derive(Debug)]
 pub enum IdxErr {
@@ -48,12 +12,9 @@ pub enum IdxErr {
     UnknownDataType,
 }
 
-
-
 pub struct IdxReader<R: Read, T> {
     source: R,
-    data_type: DataType,
-    type_phantom: PhantomData<T>,
+    data_type: PhantomData<T>,
     dimensions: Vec<u32>,
 }
 
@@ -71,35 +32,84 @@ impl<R: Read, T> IdxReader<R, T> {
             return Err(IdxErr::WrongHeader);
         }
 
+        // The goal is to instantiate an IdxReader<R, T>, where T depends on the contents of
+        // byte 3 of the Idx file. Ways that I know don't work:
+        //
+        // 1. Just `struct IdxReader<R>`. The problem with this is that we need a method of signature
+        //    `pub fn extract_next(&mut self) -> T`, which produces an appropriate T.
+        // 2. `struct IdxReader<R, T>`. The problem with this is that we don't know ahead of time which T
+        //    will be produced. No matter how many layers of indirection we go back, at some point we need
+        //    to allocate an IdxReader, but we don't know what type it will have in advance.
+        // 3. `trait IdxReader { type Item; }; impl IdxReader for ActualIdxReader`. If we could do
+        //    codegen / realtime compilation and code insertion, this approach might have promise. We'd
+        //    just emit appropriate code to hide the relevant type behind a trait, and then things would
+        //    work. Unfortunately, I know of no way to do codegen / code insertion in this language
+        //    at runtime.
+        //
+        // Ways which might work but I don't like:
+        // 1. ```
+        //    pub enum DataType {
+        //        U8(u8),
+        //        I8(i8),
+        //        ...
+        //    }
+        //    ```
+        //    We could create a struct which wrapped each data type, then always return a struct member.
+        //    However, I don't want to take that approach, for two reasons.
+        //
+        //    A. It makes the consumers of this library have to jump through some ugly hoops, matching
+        //       on the datatype whenever they want to actually use it.
+        //    B. It's insanely wasteful of space. An enum takes, bare minimum,
+        //       `1 + size_of(largest_contained_member)` bytes. This means that even when we're
+        //       actually working with simple `u8`s, each one takes at least 9 bytes of memory because the
+        //       enum has to contain `f64`s. Once aligned, that becomes 16.
+        // 2. Write a macro which duplicates implementation of `trait IdxReader { type Item; }` for
+        //    all relevant `$t:ty`, and just return an error if the header byte doesn't match the expected
+        //    type. This is the least worst option I've come up with so far, but it's still pretty bad,
+        //    just because the ergonomics are terrible. The user just has to know in advance what sort of
+        //    file it is.
+        //
+        //    There's one way that I can see around it: have the macro also write a function
+        //    with the signature `create_reader<R: Read + Clone>(reader: R) -> Result<Box<IdxReader>, IdxErr>`.
+        //    (It would be much nicer if we could return `-> Result<impl IdxReader, IdxErr>`, but that's
+        //    still yet to come, unfortunately.) This function simply attempts to create the reader using
+        //    every generated implementation, and filters out all the ones which fail. The key line
+        //    probably looks something like
+        //    `$implementations.iter().map(|i| i.try_create(reader)).filter(|result| result.is_ok())`.
+        //
+        //    I'm not sure if that's actually possible, but if it is, it might clear up a lot of the
+        //    UI pain I'm going through now, so I think I'll attack it from that direction next.
+        //
+        // I'm honestly not sure if this is a soluble problem right now in this language. I may end
+        // up producing a partial implementation specialized for `u8`s, as I still need to read some
+        // IDX files, but that's really not a satisfactory solution to the problem.
+
         // 0x08: unsigned byte
         // 0x09: signed byte
         // 0x0B: short (2 bytes)
         // 0x0C: int (4 bytes)
         // 0x0D: float (4 bytes)
         // 0x0E: double (8 bytes)
-        let dt = match byte32[2] {
-            0x08 => DataType::U8,
-            0x09 => DataType::I8,
-            0x0B => DataType::I16,
-            0x0C => DataType::I32,
-            0x0D => DataType::F32,
-            0x0E => DataType::F64,
-            _ => return Err(IdxErr::UnknownDataType),
-        };
+        // let dt = match byte32[2] {
+        //     0x08 => DataType::U8,
+        //     0x09 => DataType::I8,
+        //     0x0B => DataType::I16,
+        //     0x0C => DataType::I32,
+        //     0x0D => DataType::F32,
+        //     0x0E => DataType::F64,
+        //     _ => return Err(IdxErr::UnknownDataType),
+        // };
 
         let n_dimensions = byte32[3];
 
         let mut dimensions = Vec::with_capacity(n_dimensions as usize);
         for _ in 0..n_dimensions {
-            // update byte32 with the next 4 bytes from the input
-            try!(reader.read_exact(&mut byte32).map_err(|e| IdxErr::IOError(e)));
-            dimensions.push(casts::as_u32(byte32));
+            dimensions.push(try!(reader.extract().map_err(|e| IdxErr::IOError(e))));
         }
 
         Ok(IdxReader {
             source: reader,
-            data_type: dt,
-            type_phantom: PhantomData,
+            data_type: PhantomData,
             dimensions: dimensions,
         })
     }
